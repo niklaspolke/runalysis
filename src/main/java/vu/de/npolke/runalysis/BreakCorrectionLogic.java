b551e49773f0b38ed1b371c97370c923292ef126
Niklas Polke <niklas.polke@gmx.de>
2015-03-27 01:28:47 +0100

    add missing copyrights

--- src/main/java/vu/de/npolke/runalysis/BreakCorrectionLogic.java
+++ src/main/java/vu/de/npolke/runalysis/BreakCorrectionLogic.java
@@ -5,6 +5,17 @@ import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Copyright (C) 2015 Niklas Polke<br/>
+ * <br/>
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your option) any later version.<br/>
+ * <br/>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * @author Niklas Polke
+ */
 public class BreakCorrectionLogic {
 
 	private static int lastIndexOfList(List<?> list) {
